b2a0ffe8fa4ff57dcaf8e01cbb8c57ea479f88d6
vmandre <vmandre@gmail.com>
2023-01-05 02:04:28 +1300

    Add code base files

---
+++ src/main/java/com/vmandre/openapi/api/OpenApiServerImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.vmandre.openapi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiServerImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiServerImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmandre/openapi/api/OpenApiServerImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmandre.openapi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiServerImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
